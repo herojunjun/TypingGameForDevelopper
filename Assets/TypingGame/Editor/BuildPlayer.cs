45e8d7b451e1261fcf7562ebd6cb1824d2fce391
NHidano <yabunonodahi@yahoo.co.jp>
2017-05-21 19:07:59 +0900

    スタート時のバリア解除実装（暫定）

--- Assets/TypingGame/Editor/BuildPlayer.cs
+++ Assets/TypingGame/Editor/BuildPlayer.cs
@@ -1,16 +1,14 @@
 ﻿using UnityEditor;
 
-namespace TypingGame
-{
-	public class BuildPlayer {
-		[MenuItem ("Build/Build WebGL Player")]
-		public static void BuildWebGlPlayer()
-		{
-			var levels = new string[]{
-				"Assets/TypingGame/Scenes/GameScene.unity",
-			};
-			BuildPipeline.BuildPlayer(levels, "build_web", 
-				BuildTarget.WebGL, BuildOptions.None); 
-		}
-	}
+namespace TypingGame {
+    public class BuildPlayer {
+        [MenuItem ("Build/Build WebGL Player")]
+        public static void BuildWebGlPlayer() {
+            var levels = new string[] {
+                "Assets/TypingGame/Scenes/GameScene.unity",
+            };
+            BuildPipeline.BuildPlayer(levels, "build_web",
+                                      BuildTarget.WebGL, BuildOptions.None);
+        }
+    }
 }
