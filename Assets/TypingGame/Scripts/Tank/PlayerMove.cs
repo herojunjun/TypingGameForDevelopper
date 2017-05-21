94d7aa627e16e119ab3b71a9ec94b01e2eae5201
NHidano <yabunonodahi@yahoo.co.jp>
2017-05-22 00:59:57 +0900

    とりあえず敵を6体配置

--- Assets/TypingGame/Scripts/Tank/PlayerMove.cs
+++ Assets/TypingGame/Scripts/Tank/PlayerMove.cs
@@ -5,15 +5,26 @@ using UnityEngine.AI;
 
 public class PlayerMove : MonoBehaviour {
 
-	public Transform[] targets;
+	public Transform[] Targets;
+
+	private NavMeshAgent navMeshAgent;
+	private int nowTarget = 0;
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<NavMeshAgent> ().SetDestination (targets [0].position);
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		navMeshAgent.SetDestination (Targets [0].position);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.transform.Find ("EnemyTank") != null) {
+			Debug.Break ();
+		}
 	}
 }
