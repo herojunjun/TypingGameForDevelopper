b14377f0e4701a75a32979151d08cb02863c4224
kokokokococoa <treat.sweet.smart@gmail.com>
2017-05-22 01:01:48 +0900

    MainGameManager と _typingManager の連携仮実装

--- Assets/TypingGame/Scripts/MainGamaManager.cs
+++ Assets/TypingGame/Scripts/MainGamaManager.cs
@@ -17,16 +17,23 @@ namespace TypingGame {
             }
         }
 
+        private TypingManager _typingManager;
+        private int _damageCount;
+        private int _killCount;
+        private bool _isPlaying;
+
         /// <summary>
         /// ゲームを初期化する。
         /// </summary>
         public void Initialize() {
+            _typingManager = GameObject.Find ("GameObject").GetComponent<TypingManager> ();
         }
 
         /// <summary>
         /// ゲームを開始する。
         /// </summary>
         public void GameStart() {
+            _isPlaying = true;
         }
 
         /// <summary>
@@ -34,24 +41,54 @@ namespace TypingGame {
         /// ポーズ中のゲームをプレイする。
         /// </summary>
         public void GamePause() {
+            if (_isPlaying) {
+                _typingManager.Pause ();
+            } else {
+                _typingManager.Play ();
+            }
+
+            _isPlaying = !_isPlaying;
         }
 
         /// <summary>
         /// プレイヤーからステータスの変更を受け取る。
         /// </summary>
         public void SendPlayerStatus() {
+            _damageCount++;
+
+            if (false) {
+                // 倒されたとき
+                GameOver ();
+            }
         }
 
         /// <summary>
         /// 敵からステータスの変更を受け取る。
         /// </summary>
         public void SendEnemyStatus() {
+            if (false) {
+                // 敵が生きているとき
+                _typingManager.NextQuestion ();
+            } else {
+                // 敵を倒したとき
+                _killCount++;
+            }
         }
 
         /// <summary>
         /// GameOverしたときに呼ばれる。
         /// </summary>
         public void GameOver() {
+            _typingManager.Pause ();
+            int score = CalcScore ();
         }
+
+        private int CalcScore () {
+            int successCount = _typingManager.SuccessCount;
+            int missCount = _typingManager.MissCount;
+            var score = ((float)successCount / (successCount + missCount)) * (_killCount * 10) - (_damageCount * 5);
+            return Mathf.CeilToInt(score);
+        }
+
     }
 }
