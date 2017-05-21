eeb0142295ba373cd3f4b277b5ab7210b3c4f51a
kokokokococoa <treat.sweet.smart@gmail.com>
2017-05-21 22:58:44 +0900

    MainGameManager の追加

---
+++ Assets/TypingGame/Scripts/MainGamaManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TypingGame {
+	public class MainGameManager {
+		private static MainGameManager _instance;
+		private MainGameManager() {
+		}
+
+		public static MainGameManager Instance {
+			get {
+				if (_instance == null) {
+					_instance = new MainGameManager();
+				}
+				return _instance;
+			}
+		}
+
+		/// <summary>
+		/// ゲームを初期化する。
+		/// </summary>
+		public void Initialize() {
+		}
+
+		/// <summary>
+		/// ゲームを開始する。
+		/// </summary>
+		public void GameStart() {
+		}
+
+		/// <summary>
+		/// プレイ中のゲームをポーズする。
+		/// ポーズ中のゲームをプレイする。
+		/// </summary>
+		public void GamePause() {
+		}
+
+		/// <summary>
+		/// GameOverしたときに呼ばれる。
+		/// </summary>
+		public void GameOver() {
+		}
+	}
+}
