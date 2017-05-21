4c3e144e4f0f6af81b645242ab858958ccf55277
NHidano <yabunonodahi@yahoo.co.jp>
2017-05-22 00:29:03 +0900

    navigationMeshをBake。仮で移動スクリプト作成

---
+++ Assets/TypingGame/Scripts/Tank/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour {
+
+	public Transform[] targets;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<NavMeshAgent> ().SetDestination (targets [0].position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
