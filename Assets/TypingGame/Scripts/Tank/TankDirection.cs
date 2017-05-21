9139625eeb6b9c52f668127b79e94993bf85c22a
kokokokococoa <treat.sweet.smart@gmail.com>
2017-05-21 16:57:48 +0900

    砲台を回転させる

---
+++ Assets/TypingGame/Scripts/Tank/TankDirection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TypingGame {
+    public class TankDirection : MonoBehaviour {
+        private GameObject _tankTurret;
+        private GameObject _enemy;
+
+        void Start () {
+            _tankTurret = gameObject.transform.Find("TankRenderers").Find ("TankTurret").gameObject;
+            _enemy = GameObject.Find ("Tank");
+        }
+
+        void Update () {
+            if (_tankTurret == null) {
+                Debug.Log ("_tankTurret is Not found");
+                return;
+            }
+
+            if (_enemy == null) {
+                Debug.Log ("_enemy is Not found");
+                return;
+            }
+
+            _tankTurret.transform.LookAt (_enemy.transform);
+        }
+    }
+}
