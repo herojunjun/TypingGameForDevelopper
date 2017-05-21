94c33a06f70200bf42c0aaa0298bce05882a51a6
NHidano <yabunonodahi@yahoo.co.jp>
2017-05-21 19:51:03 +0900

    弾を発射するshotスクリプト実装
    Enemyタグ実装
    
    TankDirection修正

---
+++ Assets/TypingGame/Scripts/Tank/Shot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class Shot : MonoBehaviour {
+
+	private Transform tankTurret;
+	public GameObject shell;
+
+	// Use this for initialization
+	void Start () {
+		tankTurret = transform.Find("TankRenderers").Find("TankTurret");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			ShotShell (GameObject.FindGameObjectWithTag("Enemy").transform);
+		}
+	}
+
+	public void ShotShell(Transform target){
+		GameObject newShell = Instantiate (shell, tankTurret.position + Vector3.up * 0.3f, tankTurret.rotation);
+		newShell.AddComponent<Rigidbody> ().useGravity = false;
+		Transform enemyTurret = target.Find ("TankRenderers").Find ("TankTurret");
+		newShell.GetComponent<Rigidbody> ().AddForce ((enemyTurret.position - newShell.transform.localPosition).normalized * 1000);
+	}
+}
